48b1ce73f5f043e88ba0c2b034e6d56f764901b9
rc-balaji <rcbalaji2003@gmail.com>
2025-01-03 17:08:40 +0530

    Finally Completed

--- apti_project/settings.py
+++ apti_project/settings.py
@@ -46,7 +46,8 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -78,8 +79,12 @@ WSGI_APPLICATION = "apti_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "testDB",
+        "USER": "root",
+        "PASSWORD": "07070707",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
--- apti_project/urls.py
+++ apti_project/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('api/', include('server.urls')),
-    path('', include('client.urls'))
+    path("aapi3/", include("server.urls")),
+    path("", include("client.urls")),
 ]
